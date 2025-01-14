d0225db7edbae54d69b17be88a181a8069cafcf8
James Grimshaw <jamesrichardgrimshaw@gmail.com>
2025-01-14 18:32:50 +1100

    add testing handling en.lua for CI pipeline

---
+++ jg-handling/en.lua
@@ -0,0 +1,24 @@
+Locales = Locales or {}
+
+Locales["en"] = {
+  back = "Back",
+  previous = "Previous",
+  next = "Next",
+  search = "Search",
+  page = "Page",
+  of = "of",
+  every = "every",
+  show = "Show",
+  delete = "Delete",
+  noSavedHandlings = "No handling configurations have been saved",
+  noResultsFound = "No results found",
+  noHandlingChangesMade = "No handling changes have been made",
+  handlingSaved = "Handling Saved",
+
+  -- Timing Tool
+  ready = "Ready",
+  recording = "Recording",
+  stopVehicle = "Stop Vehicle",
+  idle = "Idle",
+  loading = "Loading",
+}
